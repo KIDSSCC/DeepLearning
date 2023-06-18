3b74c2855c71493080500920e3330d2fca39885b
KIDSSCC <18147667093@163.com>
2023-06-18 19:52:38 +0800

    CNN_baseline

---
+++ CNN/resnet.py
@@ -0,0 +1,7 @@
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torchvision
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
